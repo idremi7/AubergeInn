2faf9e51ab185ecce380ae6041b4d6f943341c6c
Rémi Létourneau <Remi.Letourneau@Usherbrooke.ca>
2021-06-08 16:02:56 -0400

    Ajouter fichier creation, destruction .sql

---
+++ creation.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Client
+(
+    idClient SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Chambre
+(
+    idChambre SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    prixBase DECIMAL NOT NULL
+);
+
+CREATE TABLE ReserveChambre
+(
+    idClient INTEGER REFERENCES Client(idClient),
+    idChambre INTEGER REFERENCES Chambre(idChambre),
+    DateDebut DATE NOT NULL,
+    DateFin DATE NOT NULL,
+    CONSTRAINT pk_clientChambre PRIMARY KEY(idClient,idChambre),
+    CONSTRAINT date CHECK (DateDebut <= DateFin)
+);
+
+CREATE TABLE Commodite
+(
+    idCommodite SERIAL PRIMARY KEY,
+    Description VARCHAR(255) NOT NULL,
+    Prix DECIMAL NOT NULL
+);
+
+CREATE TABLE PossedeCommodite
+(
+    idCommodite INTEGER REFERENCES Commodite(idCommodite),
+    idChambre INTEGER REFERENCES Chambre(idChambre),
+    CONSTRAINT pk_commoditeChambre PRIMARY KEY(idCommodite,idChambre)
+);
