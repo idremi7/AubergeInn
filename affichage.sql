73bfa5a1441ec02f6b4a2cf593bd8bf6f44a7716
pieyo <godp2203@usherbrooke.ca>
2021-06-15 19:14:58 -0400

    test

--- affichage.sql
+++ affichage.sql
@@ -3,3 +3,11 @@ SELECT * FROM chambre;
 SELECT * FROM reservechambre;
 SELECT * FROM commodite;
 SELECT * FROM possedecommodite;
+
+Select  r.*,(c.prixbase + coalesce(SUM(co.prix),0)) as prixTotal
+from reservechambre r
+         JOIN chambre c on r.idchambre = c.idchambre
+         LEFT JOIN possedecommodite p on c.idchambre = p.idchambre
+         LEFT JOIN commodite co on co.idcommodite = p.idcommodite
+where r.idclient =1
+GROUP BY c.prixbase, r.idreservation;
